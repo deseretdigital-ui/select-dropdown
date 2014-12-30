3b3f5144c3a79d5a4e9cf3e1de5bc416e58f95d6
Joshua Stoutenburg <jehoshua02@gmail.com>
2014-12-30 13:25:44 -0700

    Copy Source into Example Bower Components
    When the example is inspected, I want it to look as though the lib was
    installed using bower to encourage the use of bower.

--- gulp/tasks/example/dist.js
+++ gulp/tasks/example/dist.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 gulp.task('example/dist', function () {
   var config = {
     src: ['./src/select-dropdown.js'],
-    dest: './example/build'
+    dest: './example/build/bower_components/select-dropdown/src'
   };
 
   return gulp.src(config.src).pipe(gulp.dest(config.dest));
